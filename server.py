ddcd945bf8bd767a1e9a00c772d0d31922542100
BorisovaDaria <dashabborisova@gmail.com>
2021-11-09 22:09:55 +0300

    server log

--- server.py
+++ server.py
@@ -1,20 +1,52 @@
-import socket
+import socket, time
+from enum import Enum
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+def main():
+	sock = socket.socket()
+	sock.bind(('', 9090))
+	log(LogType.SERVER_STARTED)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	sock.listen(0)
+	log(LogType.START_LISTEN)
 
-print(msg)
+	conn, addr = sock.accept()
+	log(LogType.CLIENT_CONNECTED, addr)
 
-conn.close()
+	msg = ''
+
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		log(LogType.DATA_RECEIVED)
+		msg += data.decode()
+		conn.send(data)
+		log(LogType.DATA_SENT)
+
+	print(msg)
+
+	conn.close()
+	log(LogType.CLIENT_DISCONNECTED)
+
+
+class LogType(Enum):
+	SERVER_STARTED = "Server started"
+	START_LISTEN = "Start listening"
+	CLIENT_CONNECTED = "Client connected"
+	DATA_RECEIVED = "Data received"
+	DATA_SENT = "Data sent"
+	CLIENT_DISCONNECTED = "Client disconnected"
+	SERVER_STOPPED = "Server stopped"
+
+
+def log(log_type: LogType, ip: tuple = None):
+	if ip != None:
+		print("[" + log_type.value + "; " + str(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())) + "; " + str(ip) + "]")
+	else:
+		print("[" + log_type.value + "; " + str(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())) + "]")
+
+
+
+if __name__ == '__main__':
+	main()
